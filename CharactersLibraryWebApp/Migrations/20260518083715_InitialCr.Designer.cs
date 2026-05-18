6c2fe2f59e7046c3bc2c8d9eb3832e8b4805a6f3
Reyzzel <demc.valeria@gamil.com>
2026-05-18 10:34:11 +0300

    Add new migration and update timestamp behavior
    Added auto-generated migration files, defining the database schema with entities, relationships, and seed data. Changed time zone type in Characters table. Updated LibraryContextModelSnapshot` to reflect this change.

--- CharactersLibraryWebApp/Migrations/20260518083715_InitialCr.Designer.cs
+++ CharactersLibraryWebApp/Migrations/20260518083715_InitialCr.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CharactersLibraryWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CharactersLibraryWebApp.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20260518083715_InitialCr")]
+    partial class InitialCr
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CharactersLibraryWebApp/Program.cs
+++ CharactersLibraryWebApp/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 // Add services to the container.
 
 builder.Services.AddControllers();
